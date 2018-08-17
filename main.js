a9689fee1a261c582b20fb8c211ed10ac456ad8d
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-08-17 09:00:40 +0900

    [update]大まかな部分の完成

---
+++ main.js
@@ -0,0 +1,24 @@
+window.onload = ()=>{
+  document.addEventListener("mousemove",fn);
+}
+
+fn = (e)=>{
+  var z = [0.05,0.08,0.17,0.2,0.10,0.15,0.16];
+  let mX = e.pageX - window.innerWidth/2;
+  let mY = e.pageY - window.innerHeight/2;;
+  
+  let elements = document.getElementsByClassName('Item');
+  
+  for(let i=0;i<elements.length;i++){
+    elements[i].style.transform = 'translate('+(-mX*z[i])+'px,'+(-mY*z[i])+'px)';
+  }
+}
+
+display = (name)=>{
+  var element = document.getElementsByClassName(name);
+  element[0].style.display = 'block';
+}
+
+windowsClose = (a)=>{
+  a.parentNode.style.display = "none";
+}
