c6553e75a0f50549c479ceced91a39f7a6ada12a
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-08-18 10:41:21 +0900

    [update]リサイズ時にタップによる移動をオン・オフするように変更

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ window.onload = ()=>{
   }
 }
 
+window.onresize = ()=>{
+  if(769 <= window.innerWidth){
+    //widhtが768pxのときだけマウス追従有効化
+    document.addEventListener("mousemove",fn);
+  }else{
+    document.removeEventListener("mousemove",fn);
+    let elements = document.getElementsByClassName('Item');
+    
+    for(let i=0;i<elements.length;i++){
+      elements[i].style.transform = 'translate(0px,0px)';
+    }
+  }
+}
+
 fn = (e)=>{
   var z = [0.05,0.08,0.17,0.2,0.10,0.15,0.16];
   let mX = e.pageX - window.innerWidth/2;
